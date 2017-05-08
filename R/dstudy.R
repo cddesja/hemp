904df3a22e734ee59263570255d6349fba7b1cbf
Christopher David Desjardins <cddesjardins@gmail.com>
2017-05-08 13:28:36 -0500

    added data gstudy can handle fixed design

--- R/dstudy.R
+++ R/dstudy.R
@@ -3,45 +3,50 @@
 #' @description
 #' Generates the variance components and the relative magnitude (percent of total variance) of each variance component.
 #'
-#' @param x An lmer fitted object
+#' @param x A gstudy object
 #' @param n A vector with facets named
 #' @param unit The label for the unit of measurement
 #'
 #' @examples
 #' ## One facet design
 #' one.facet <- lmer(Score ~ (1 | Participants) + (1 | Items), data = efData)
-#' dstudy(one.facet, n = c("Items" = 4), unit = "Participants")
+#' one.facet.gstudy <- gstudy(one.facet)
+#' dstudy(one.facet.gstudy, n = c("Items" = 4), unit = "Participants")
 #'
 #' ## Two facet design
-#' lmer.tab3.2 <- lmer(score ~ (1 | child) + (1 | raters) + (1 | occasions) + (1 | child:raters) + (1 | child:occasions) + (1 | raters:occasions), data = swtab1.3)
-#' dstudy(lmer.tab3.2, unit = "child", n = c("occasions" = 4, "raters" = 2))
+#' two.facet <- lmer(scores ~ (1 | students) + (1  | prompts) + (1 | raters) + (1 | students:prompts) +  (1 | students:raters) + (1 | prompts:raters), data = writing)
+#' two.facet.gstudy <- gstudy(two.facet)
+#' dstudy(two.facet.gstudy, n = c("raters" = 2, "prompts" = 5), unit = "students")
+#'
+#' ## Two facet fixed design wtih prompt
+#' gstudy.fixed.prompt <- gstudy(two.facet, fixed = "prompts")
+#' dstudy(gstudy.fixed.prompt, n = c("raters" = 2), unit = "students")
 #' @export
 dstudy <- function(x, n, unit) {
-  tmp <- as.data.frame(VarCorr(x))
-  tmp <- tmp[c(1,4)]
-  us.var <- tmp[tmp$grp %in% unit,2]
-
+  tmp <- x$gstudy.out
+  tmp <- tmp[c(1,2)]
+  us.var <- tmp[tmp$Source %in% unit,2]
   n.matrix <- matrix(nrow = nrow(tmp), ncol = length(n))
-  for(i in 1:length(n)) n.matrix[grep(names(n)[i], tmp$grp),i] <- n[i]
+  for(i in 1:length(n)) n.matrix[grep(names(n)[i], tmp$Source),i] <- n[i]
   n.matrix[nrow(n.matrix),] <- n
   tmp$n <- apply(n.matrix, 1, prod, na.rm = T)
-  tmp[match(unit, tmp$grp), "n"] <- length(x@resp$y)
-  tmp$vcov.n <- tmp$vcov/tmp$n
-  tmp[match(unit, tmp$grp), "vcov.n"] <- tmp[match(unit, tmp$grp), "vcov"]
+  tmp[match(unit, tmp$Source), "n"] <- x$nobs
+  tmp$vcov.n <- tmp$Est.Variance/tmp$n
+  tmp[match(unit, tmp$Source), "vcov.n"] <- tmp[match(unit, tmp$Source), "Est.Variance"]
 
   # relative variance ----
   rel.var <- tmp$vcov.n[nrow(tmp)]
   if(length(n)>1){
     for(i in 1:length(n)) {
       tmp.names <- c(paste0(unit, ":", names(n)[i]), paste0(names(n)[i], ":", unit))
-      tmp.var <- tmp[tmp$grp %in% tmp.names,"vcov.n"]
+      tmp.var <- tmp[tmp$Source %in% tmp.names,"vcov.n"]
       rel.var <- sum(rel.var, tmp.var)
     }
   }
 
   # absolute variance ----
   tmp.abs <- tmp[-nrow(tmp),]
-  abs.var <- sum(tmp.abs[-grep(unit, tmp.abs$grp),"vcov.n"], rel.var)
+  abs.var <- sum(tmp.abs[-grep(unit, tmp.abs$Source),"vcov.n"], rel.var)
 
   # generalizability coefficient
   g.coef <- us.var/(us.var + rel.var)
--- R/gstudy.R
+++ R/gstudy.R
@@ -6,15 +6,48 @@
 #' @param x An lmer fitted object
 #'
 #' @examples
-#' mod <- lmer(score ~ (1 | participant) + (1 | item), data = efData)
-#' gstudy(mod)
+#' one.facet <- lmer(Score ~ (1 | Participants) + (1 | Items), data = efData)
+#' two.facet <- lmer(scores ~ (1 | students) + (1  | prompts) + (1 | raters) + (1 | students:prompts) +  (1 | students:raters) + (1 | prompts:raters), data = writing)
+#' gstudy(one.facet)
+#' gstudy(two.facet)
+#' # Two facet with prompts fixed
+#' gstudy(two.facet, fixed = "prompts")
 #' @export
-gstudy <- function(x) {
+gstudy <- function(x, fixed = NULL) {
   tmp <- as.data.frame(VarCorr(x))
   tmp <- tmp[c(1,4)]
+  no.match <- function(x) {x[-match(fixed, x)]}
+  if(!is.null(fixed)){
+    n_adj <- length(unique(x@frame[,grep(fixed, names(x@frame))]))
+    fixed_vars <- tmp[grep(fixed, tmp$grp),]
+    fixed_vars <- fixed_vars[-match(fixed, fixed_vars$grp),]
+    fixed_vars$adj_vcov <- fixed_vars$vcov/n_adj;fixed_vars$vcov <- NULL
+    add_back <- strsplit(fixed_vars$grp, ":")
+    fixed_vars$grp <- sapply(add_back, no.match)
+    two.way <- data.frame(grp = paste(fixed_vars$grp, collapse = ":"), adj_vcov = tmp[nrow(tmp),2]/n_adj)
+    fixed_vars <- rbind(fixed_vars, two.way)
+    tmp <- merge(tmp, fixed_vars)
+    tmp[,2] <- tmp[,2] + tmp[,3]; tmp[,3] <- NULL
+    tmp$grp[length(tmp$grp)] <- "Residual"
+    }
   colnames(tmp) <- c("Source", "Est.Variance")
   tmp$Percent.Variance <- tmp$Est.Variance/sum(tmp$Est.Variance)
   tmp[,2] <- round(tmp[,2], 4)
   tmp[,3] <- paste0(round(tmp[,3] * 100,1), "%")
-  return(tmp)
-  }
+  N <- length(x@resp$y)
+  output <- list(gstudy.out = tmp, nobs = N)
+  class(output) <- "gStudy"
+  return(output)
+}
+
+#' Prints the results from the G-study
+#'
+#' Prints  output from fitting the \code{gstudy} function.
+#' @param x object of class \code{gStudy} returned from the \code{gstudy} function
+#' @param ... additional objects of the same type.
+#' @method print gStudy
+#' @seealso \code{\link{gstudy}}
+#' @export
+print.gStudy <- function(x, ...){
+  print(x$gstudy.out)
+}
