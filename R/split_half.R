71cd1972d5a3bc11b0e69ab4e71131d24b61c28a
Christopher David Desjardins <cddesjardins@gmail.com>
2017-10-27 14:34:55 -0700

    Added Spearman-Brown Correction

--- R/split_half.R
+++ R/split_half.R
@@ -1,16 +1,17 @@
-#' @title Split half reliability
+#' @title Split-half reliability
 #' @description
 #' Calculates split-half reliability either by selecting every other item or by randomly split the items.
 #'
 #' @param data A data frame
 #' @param type Either alternate or random. Default is alternate.
-#' @return The split half reliability is caculated using every other item, if \code{type = "alternate"} is specified, or by splitting the instrument in half at random, if \code{type = "random"} is specifed.
+#' @param sb Use the Spearman-Brown correction? Default is FALSE.
+#' @return The split half reliability is calculated using every other item, if \code{type = "alternate"} is specified, or by splitting the instrument in half at random, if \code{type = "random"} is specifed. If \code{sb = TRUE}, then the Spearman-Brown correction is returned.
 #' @examples
 #' data(SAPA)
 #' split_half(SAPA, type = "alternate")
 #' split_half(SAPA, type = "random")
 #' @export
-split_half <- function(data, type = "alternate") {
+split_half <- function(data, type = "alternate", sb = FALSE) {
   if(type != "alternate" & type != "random"){
     stop('Please specify type = "alternate" or type = "random".')
   }
@@ -19,8 +20,8 @@ split_half <- function(data, type = "alternate") {
     second_half <- data[, seq(2, ncol(data), by = 2)]
     first_total <- rowSums(first_half, na.rm = T)
     second_total <- rowSums(second_half, na.rm = T)
-    reliability <- cor(first_total, second_total)
-    return(round(reliability, 3))
+    rel <- cor(first_total, second_total)
+    rel_rd <- round(rel, 3)
   }
   if(type == "random"){
     num_items <- 1:ncol(data)
@@ -29,8 +30,12 @@ split_half <- function(data, type = "alternate") {
     second_half <- data[, -first_items]
     first_total <- rowSums(first_half, na.rm = T)
     second_total <- rowSums(second_half, na.rm = T)
-    reliability <- cor(first_total, second_total)
-    return(round(reliability, 3))
+    rel <- cor(first_total, second_total)
+    rel_rd <- round(rel, 3)
   }
+  if(sb == TRUE){
+    rel_rd <- 2 * rel_rd / (1 + rel_rd)
+  }
+  return(rel_rd)
 }
 
