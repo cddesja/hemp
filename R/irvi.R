9e73dc5ee9b52ad4edf50081e0008da168fbd06c
Christopher David Desjardins <cddesjardins@gmail.com>
2017-10-29 09:49:16 -0700

    fixed irvi error

--- R/irvi.R
+++ R/irvi.R
@@ -2,10 +2,10 @@
 #' @description
 #' Calculates the item-reliability index or the item-validity index
 #'
-#' @param data
+#' @param data data frame
 #' @param use How should missing data be handled? See the \code{cor} function. Default is "complete.obs"
 #' @param na.rm logical. Should missing values be removed? Defualt is TRUE.
-#' @return CVR.
+#' @return Either the item-reliability or item-validity index
 #' @examples
 #' cvr(20, 17)
 #' @export
@@ -13,7 +13,6 @@ irvi <- function(data, use = "complete.obs", na.rm = T){
   ttscore <- rowSums(data, na.rm = na.rm)
   s_i <- apply(data, 2, sd, na.rm = na.rm)
   r <- cor(data, ttscore, use = use)
-  r <- r[-length(r)]
   index <- s_i * r
   return(index)
 }
