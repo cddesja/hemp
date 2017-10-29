9c5a193d4fe8e0f366bd5b63c3633b5c6eff6e50
Christopher David Desjardins <cddesjardins@gmail.com>
2017-10-29 09:30:27 -0700

    changed SAPA to data.frame and fixed CVR

--- R/cvr.R
+++ R/cvr.R
@@ -3,13 +3,13 @@
 #' Calculates CVR
 #'
 #' @param N total number of experts
-#' @param Ne number of experts that agree that the item is essential.
+#' @param n_e number of experts that agree that the item is essential.
 #' @return CVR.
 #' @examples
 #' cvr(20, 17)
 #' @export
 
-cvr <- function(N, Ne){
+cvr <- function(N, n_e){
   value <- (n_e - (N / 2)) / (N / 2)
   return(value)
 }
