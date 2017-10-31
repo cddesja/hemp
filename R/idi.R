48cd2d770e6e326ac036e87747daea11e6361051
Christopher David Desjardins <cddesjardins@gmail.com>
2017-10-31 16:52:57 -0700

    added the options to specify percentile in idi

--- R/idi.R
+++ R/idi.R
@@ -4,20 +4,21 @@
 #'
 #' @param data data frame
 #' @param item An item (as a vector)
+#' @param perc_cut A number that defines the upper and lower percentile to use for the high and low achieving groups
 #' @param use How should missing data be handled? See the \code{cor} function. Default is "complete.obs"
 #' @param na.rm logical. Should missing values be removed? Defualt is TRUE.
 #' @return The item discrimination index
 #' @examples
-#' idi(SAPA, SAPA$reason.4)
+#' idi(SAPA, SAPA$reason.4, perc_cut = .27)
 #' @export
-idi <- function(data, item, use = "complete.obs", na.rm = T){
+idi <- function(data, item, perc_cut, use = "complete.obs", na.rm = T){
   ttscore <- rowSums(data, na.rm = na.rm)
-  score_cuts <- quantile(ttscore, probs = c(.27, .73), na.rm = T)
+  score_cuts <- quantile(ttscore, probs = c(perc_cut, 1 - perc_cut), na.rm = T)
   group <- ifelse(ttscore <= score_cuts[1], "low",
               ifelse(ttscore <= score_cuts[2], "high", "middle"))
   # print the results
   diff_tab <- table(group, item)[-3,]
   tab <- prop.table(diff_tab, 2)[,2]
-  names(tab) <- c("Upper 27%", "Lower 27%")
+  names(tab) <- paste0(c("Upper ", "Lower "), perc_cut * 100, "%")
   return(tab)
 }
